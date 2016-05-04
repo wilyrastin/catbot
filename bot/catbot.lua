5341042446241d2e8c7930cc5fc85c37606934ad
wilyrastin <wilyrastin0098@gmail.com>
2016-05-04 08:50:25 +0430

    Update and rename seedbot.lua to catbot.lua

--- bot/catbot.lua
+++ bot/catbot.lua
@@ -226,30 +226,13 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {205903314},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+    about_text = [
 
-https://github.com/SEEDTEAM/TeleSeed
+https://github.com/wilyrastin/catbot
 
-Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
-
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
-
-Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
 ]],
     help_text_realm = [[
 Realm Commands:
